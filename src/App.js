1b9f4e8648e3309c3f94e402b8f2c865fef76ad1
alyssa rodriguez <amrrodriguez@ucdavis.edu>
2023-10-28 18:07:48 -0700

    added a loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './variables.css';
+import { useState, useEffect } from 'react';
 import CustomNavbar from './components/navbar/Navbar';
 import Home from './sections/home/Home';
 import Shows from './sections/shows/Shows';
@@ -8,21 +9,50 @@ import Music from './sections/music/Music';
 import Contact from './sections/contact/Contact';
 import Footer from './components/footer/Footer';
 import HorizontalLine from './components/horizontal-line/HorizontalLine';
+import ScaleLoader from "react-spinners/ScaleLoader";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  // going to server as lifecycle method
+  // passing in a '[]' meaning function will run one time when the page is rendered
+  // when the page is rendered, set loading to true 
+  useEffect(() => {
+    setLoading(true);
+    
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000)
+  }, [])
+
   return (
     <div className="App">
-      <CustomNavbar />
-      <Home />
-      <HorizontalLine />
-      <Shows />
-      <HorizontalLine />
-      <Videos embedId="L_bkwHCTjqA" />
-      <HorizontalLine />
-      <Music />
-      <HorizontalLine />
-      <Contact />
-      <Footer />
+      {/* display loader if loading is true, else display app */}
+      {
+        loading ? (
+          <ScaleLoader
+          color={"red"}
+          loading={loading}
+          radius={"5"}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          />
+        ) : (
+          <div>
+            <CustomNavbar />
+            <Home />
+            <HorizontalLine />
+            <Shows />
+            <HorizontalLine />
+            <Videos embedId="L_bkwHCTjqA" />
+            <HorizontalLine />
+            <Music />
+            <HorizontalLine />
+            <Contact />
+            <Footer />
+          </div>
+        )
+      }
     </div>
   );
 }
