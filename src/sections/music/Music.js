27d378cdda3c5225981f49accd35297b3582f1c5
alyssa rodriguez <amrrodriguez@ucdavis.edu>
2023-11-08 20:55:04 -0800

    add a hover animation for music imgs

--- src/sections/music/Music.js
+++ src/sections/music/Music.js
@@ -1,14 +1,33 @@
 import styles from './Music.module.css';
+import { motion } from 'framer-motion';
 import SavyPhoto2 from '../../assets/photos/savy/savy-cover-art.jpg';
 import Pity from '../../assets/photos/pity/pity.png';
+
+// animation variants
+const musicVariants = {
+    hover: {
+        scale: 1.1
+    }
+}
 
 const Music = () => {
     return (
         <section className={ styles.Music } id="music">
-            <div id={ styles.musicContainer }>
-                <img src={ SavyPhoto2 } alt="Savy2"/>
-                <img src={ Pity } alt="Pity"/>
-            </div>
+            <motion.div 
+                id={ styles.musicContainer }>
+                    <motion.img 
+                        variants={ musicVariants }
+                        whileHover="hover"
+                        src={ SavyPhoto2 } 
+                        alt="Savy2"
+                    />
+                    <motion.img 
+                        variants={ musicVariants }
+                        whileHover="hover"
+                        src={ Pity }
+                        alt="Pity"
+                    />
+            </motion.div>
         </section>
     );
 }
