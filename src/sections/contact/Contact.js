772b5fde119dfcdc7ebdd57bf1b0bde79e960448
alyssa rodriguez <amrrodriguez@ucdavis.edu>
2023-11-15 12:58:23 -0800

    added animation to contact section

--- src/sections/contact/Contact.js
+++ src/sections/contact/Contact.js
@@ -1,21 +1,53 @@
 import styles from './Contact.module.css';
+import { motion } from 'framer-motion';
 import KittyDoodle from '../../assets/doodles/kitty-transparent.png';
+
+// animation variants
+const contactVariants = {
+    hidden: {
+        opacity: 0
+    },
+    visible: {
+        opacity: 1,
+        transition: { 
+            delay: 1,
+            duration: 1.5
+        }
+    }
+}
 
 const Contact = () => {
     return (
         <section className={ styles.Contact } id="contact">
             <div id={ styles.contactContainer }>
-                <h2>management:<br/>alyssa rodriguez</h2>
-                <h2>booking:<br/><a href="mailto:booklunaivy@gmail.com">booklunaivy@gmail.com</a></h2>
+                <motion.h2
+                    variants={ contactVariants }
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}>
+                        management:<br/>alyssa rodriguez
+                </motion.h2>
+                <motion.h2
+                    variants={ contactVariants }
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}>
+                        booking:<br/><a href="mailto:booklunaivy@gmail.com">booklunaivy@gmail.com</a>
+                </motion.h2>
             </div>
-            <div id={ styles.doodleContainer }>
+            <motion.div 
+                id={ styles.doodleContainer }
+                variants={ contactVariants }
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}>
                 <img
                     src={ KittyDoodle }
                     width="275px"
                     height="380px"
                     alt="KittyDoodle"
                 />
-            </div>
+            </motion.div>
         </section>
     );
 }
