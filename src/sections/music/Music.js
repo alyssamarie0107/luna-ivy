445c27e33a620810b7afe48a45cebaf94c72490d
alyssa rodriguez <amrrodriguez@ucdavis.edu>
2023-11-18 09:12:50 -0800

    added beetle doodles to music section and applied some animation

--- src/sections/music/Music.js
+++ src/sections/music/Music.js
@@ -1,5 +1,6 @@
 import styles from './Music.module.css';
 import { motion } from 'framer-motion';
+import Beetle from '../../assets/doodles/beetle.png';
 import SavyPhoto2 from '../../assets/photos/savy/savy-cover-art.jpg';
 import Pity from '../../assets/photos/pity/pity.png';
 
@@ -23,6 +24,31 @@ const musicVariants = {
 const Music = () => {
     return (
         <section className={ styles.Music } id="music">
+            <div id={ styles.musicHeader }>
+                <motion.img 
+                    src={ Beetle } 
+                    alt="Beetle Doodle"
+                    variants={ musicVariants }
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                />
+                <motion.h1
+                    variants={ musicVariants }
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}>
+                        music
+                </motion.h1>
+                <motion.img 
+                    src={ Beetle } 
+                    alt="Beetle Doodle"
+                    variants={ musicVariants }
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                />
+            </div>
             <motion.div 
                 id={ styles.musicContainer }>
                     <motion.img 
