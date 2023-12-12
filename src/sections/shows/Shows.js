aae3922eafde534bf786d84ab9ac1236f0e5642c
alyssa rodriguez <amrrodriguez@ucdavis.edu>
2023-12-11 17:24:37 -0800

    remove neck of the woods and added new years show

--- src/sections/shows/Shows.js
+++ src/sections/shows/Shows.js
@@ -36,20 +36,20 @@ const Shows = () => {
                     <ListGroup>
                         <ListGroup.Item className={ styles.listGroup }>
                             <div className={ styles.DateVenue }>
-                                <p>Dec. 10, 2023</p>
-                                <p>Neck of the Woods</p>
-                            </div>
-                            <div className= { styles.City }>
-                                <p>San Francisco, CA</p>
-                            </div>
-                        </ListGroup.Item>
-                        <ListGroup.Item className={ styles.listGroup }>
-                            <div className={ styles.DateVenue }>
                                 <p>Dec. 22, 2023</p>
                                 <p>House Show</p>
                             </div>
                             <div className= { styles.City }>
                                 <p>Oakland, CA</p>
+                            </div>
+                        </ListGroup.Item>
+                        <ListGroup.Item className={ styles.listGroup }>
+                            <div className={ styles.DateVenue }>
+                                <p>Dec. 31, 2023</p>
+                                <p>New Years House Show</p>
+                            </div>
+                            <div className= { styles.City }>
+                                <p>San Francisco, CA</p>
                             </div>
                         </ListGroup.Item>
                     </ListGroup>
